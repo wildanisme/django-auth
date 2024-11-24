1fcff7e9a5a2af05790949102dd6d355b57b8ce6
wildanisme <wildanisme.com@@gmail.com>
2024-11-24 19:51:42 +0700

    create register

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CustomLoginView, HomeView, CustomLogoutView
+from .views import CustomLoginView, HomeView, CustomLogoutView, RegisterView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('home/', HomeView.as_view(), name='home'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- my_auth/views.py
+++ my_auth/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.contrib.auth import login, authenticate
+
+from .forms import SignUpForm
+
+
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
@@ -15,3 +21,23 @@ class HomeView(View):
 class CustomLogoutView(LogoutView):
     template_name = 'accounts/login.html'
     next_page = 'login'
+
+
+class RegisterView(View):
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+
+        return render(request, self.template_name, {'form': form})
